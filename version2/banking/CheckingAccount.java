0c2376135b2b183c1b15b069d11c7de41b360a81
Alvin Macion <macion.et.al@gmail.com>
2022-07-19 14:43:30 -0700

    version2
    submission for version2

---
+++ version2/banking/CheckingAccount.java
@@ -0,0 +1,38 @@
+ package banking;
+
+ public class CheckingAccount extends Account
+ {
+ 	private SavingsAccount protectedBy;
+
+ 	public CheckingAccount(double balance)
+ 	{
+ 		super(balance);
+ 	}
+
+    public CheckingAccount(double balance, SavingsAccount protect)
+    {
+        super(balance);
+        this.protectedBy = protect;
+    }
+    
+    @Override
+    public boolean withdraw(double amt)
+    {
+    boolean flag = false;
+        if(amt <= super.getBalance()) 
+        {
+            flag = super.withdraw(amt);
+        }
+        else if(this.protectedBy != null)
+        {
+            double difference = amt - super.getBalance();
+            if(difference <= this.protectedBy.getBalance())
+            {
+                flag = super.withdraw(super.getBalance());
+                this.protectedBy.withdraw(difference);
+            }
+        }
+
+        return flag;
+    }
+ }
