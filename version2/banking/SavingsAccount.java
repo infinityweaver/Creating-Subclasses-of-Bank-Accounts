56a4ae95f78b979a6ef53db787b7d20a0047bb75
RubelynAris <108714983+RubelynAris@users.noreply.github.com>
2022-07-19 15:32:10 +0800

    Lab3

---
+++ version2/banking/SavingsAccount.java
@@ -0,0 +1,14 @@
+package banking;
+
+public class SavingsAccount extends Account
+{
+	double interestRate;
+
+
+	public SavingsAccount(double balance, double interest_rate)
+	
+	{
+		super(balance);
+		this.interestRate = interest_rate;
+	}
+} 
