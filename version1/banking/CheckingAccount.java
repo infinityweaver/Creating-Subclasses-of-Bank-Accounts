7e65432ca8f6e37d4b91e08419c28f9be1dc2340
cassiophe <gucelarysha@gmail.com>
2022-07-19 16:14:42 +0800

    gucela lab 3
    version 1 exe 1 - 2
    version 2 exe 1 - 3

---
+++ version1/banking/CheckingAccount.java
@@ -0,0 +1,39 @@
+package banking;
+
+public class CheckingAccount extends Account
+{
+	private double overdraftProtection;
+
+	public CheckingAccount(double balance)
+	{
+		super(balance);
+	}
+
+	public CheckingAccount(double balance, double protect)
+	{
+		super(balance);
+		this.overdraftProtection = protect;
+	}
+
+	@Override
+	public boolean withdraw(double amt)
+	{
+		boolean flag = false;
+		if(amt <= super.getBalance())
+		{
+			flag = super.withdraw(amt);
+		}
+		else if(amt <= super.getBalance() + this.overdraftProtection)
+		{
+			flag = super.withdraw(amt);
+			double difference = amt - super.getBalance();
+			
+			if(difference <= this.overdraftProtection)
+			{
+				flag = super.withdraw(super.getBalance());
+				this.overdraftProtection -= difference;
+			}
+		}
+		return flag;
+	}
+}
