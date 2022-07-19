b6dd78adc9c32d28afcc75ea27ea3f76ba261c4d
KaelNierras <kyleanthony47@gmail.com>
2022-07-19 15:05:34 +0800

    Kyle Anthony F. Nierras Version 1 and 2

---
+++ TestBanking.java
@@ -0,0 +1,7 @@
+package banking;
+
+public class Customer
+{
+
+
+}
---
+++ version1/banking/CheckingAccount.java
@@ -0,0 +1,38 @@
+package banking;
+
+public class CheckingAccount extends Account
+{
+	private double overdraftProtection;
+
+	public CheckingAccount(double balance)
+	{
+		super(balance);
+	}
+
+	public CheckingAccount(double balance, double protect)
+	{
+		super(balance);
+		this.overdraftProtection = protect;
+	}
+
+	@Override
+	public boolean withdraw(double amt)
+	{
+		boolean flag = false;
+		if(amt <= super.getBalance())
+		{
+			flag = super.withdraw(amt);
+		}
+
+		else if (this.overdraftProtection > 0)
+		{
+			double difference = amt - super.getBalance();
+			if(difference <= this.overdraftProtection)
+			{
+				flag = super.withdraw(super.getBalance());
+			}
+		}
+
+		return flag;
+	}
+}
